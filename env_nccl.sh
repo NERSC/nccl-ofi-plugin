3e9e928d5e28a1afd140c061bec34d5b1953032b
Steve Farrell <sparticlesteve@gmail.com>
2022-12-05 13:07:08 -0800

    runtime environment and tests

--- env_nccl.sh
+++ env_nccl.sh
@@ -1,9 +1,18 @@
 export INSTALL_DIR=`pwd`/install
 export LD_LIBRARY_PATH=$INSTALL_DIR/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$INSTALL_DIR/deps/lib:$LD_LIBRARY_PATH
+
+export FI_CXI_DISABLE_HOST_REGISTER=1
 export NCCL_CROSS_NIC=1
 export NCCL_DEBUG=INFO
 export NCCL_SOCKET_IFNAME=hsn
+export NCCL_NET_GDR_LEVEL=PHB
 
 # If you want it to fail when plugin load unsuccesful
 export NCCL_NET="AWS Libfabric"
+
+# A short term workaround to some Linux kernel setting HPE says needs to be set but isn't.
+export FI_MR_CACHE_MONITOR=memhooks
+
+# You may also need this
+#export LD_PRELOAD=$INSTALL_DIR/deps/lib/libcrypto.so.1.1:$INSTALL_DIR/deps/lib/libssl.so.1.1
--- run_tests.sh
+++ run_tests.sh
@@ -8,36 +8,21 @@
 #SBATCH --time=10 
 #SBATCH -o slurm-test-%j.out
 
-module load libfabric/1.15.0.0
 module load cudatoolkit/11.7
-module load craype-accel-nvidia80
 
 export NCCL_HOME=$PWD/install
-export N=10
-export CC=cc
-export CXX=CC
-export MPICC=CC
-export MPI_HOME=$CRAY_MPICH_DIR
 export MPICH_GPU_SUPPORT_ENABLED=1
-export CRAY_ACCEL_TARGET=nvidia80
-export NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80"
 
 export LD_LIBRARY_PATH=$NCCL_HOME/lib:$LD_LIBRARY_PATH
+export FI_CXI_DISABLE_HOST_REGISTER=1
 export NCCL_CROSS_NIC=1
 export NCCL_DEBUG=INFO
 export NCCL_SOCKET_IFNAME=hsn
 export NCCL_NET="AWS Libfabric"
+export NCCL_NET_GDR_LEVEL=PHB
 
-echo ========== BUILDING NCCL TESTS ==========
-if [ ! -e nccl-tests ]; then
-    git clone https://github.com/NVIDIA/nccl-tests.git
-    cd nccl-tests
-    #git checkout 8274cb4 
-    make -j $N MPI=1
-    cd ..
-else
-    echo Skipping ... nccl-tests directory already exists
-fi
+# A short term workaround to some Linux kernel setting HPE says needs to be set but isn't.
+export FI_MR_CACHE_MONITOR=memhooks
 
 echo ========== RUNNING NCCL TESTS ==========
 srun nccl-tests/build/all_reduce_perf -b 8 -e 4G -f 2
